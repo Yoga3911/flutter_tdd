a3a0c46cc584850e1264672c320f5cb635176476
Robin Hood <yoga3911@gmail.com>
2022-07-07 00:29:51 +0700

    add insert product injection

--- lib/injection.dart
+++ lib/injection.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:my_project/app/features/product/data/repositories/product_data_repository_impl.dart';
 import 'package:my_project/app/features/product/domain/usecases/get_product_data_usecase.dart';
+import 'package:my_project/app/features/product/domain/usecases/insert_product_usecase.dart';
 
 import 'app/features/product/data/sources/product_data_remote.dart';
 import 'app/features/product/domain/repositories/product_data_repository.dart';
@@ -29,6 +30,12 @@ Future<void> setup() async {
   //! Use Case
   getIt.registerLazySingleton(
     () => GetProductDataUseCase(
+      repository: getIt(),
+    ),
+  );
+
+  getIt.registerLazySingleton(
+    () => InsertProductUseCase(
       repository: getIt(),
     ),
   );
