e080b75f26255b9f42a8f998f1e2acfc98ca9047
Robin Hood <yoga3911@gmail.com>
2022-07-07 00:31:26 +0700

    move params class into core/usecase

--- lib/app/core/usecase/usecase.dart
+++ lib/app/core/usecase/usecase.dart
@@ -7,6 +7,16 @@ abstract class UseCase<Type, Params> {
   Future<Either<Failure, Type>> call(Params params);
 }
 
+class Params extends Equatable {
+  final int number;
+
+  const Params({required this.number});
+
+  @override
+  List<Object?> get props => [number];
+}
+
+
 class NoParams extends Equatable {
   @override
   List<Object?> get props => [];
