3bbe5c62df4c17bd2b5771d9824efe910dfb1a27
Robin Hood <yoga3911@gmail.com>
2022-07-07 00:31:39 +0700

    update insert product model

--- lib/app/features/product/data/models/insert_product_model.dart
+++ lib/app/features/product/data/models/insert_product_model.dart
@@ -7,4 +7,15 @@ class InsertProductModel extends InsertProductEntity {
     required super.quantity,
     required super.description,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      "name": productName,
+      "price": price,
+      "quantity": quantity,
+      "description": description,
+      "user_id": "fc8445b1-9a74-4416-969c-2c69e5dc5686",
+      "category_id": 2,
+    };
+  }
 }
