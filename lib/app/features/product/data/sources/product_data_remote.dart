b165de8df8cae7cd8b4596d4713a8a2c3bd260dc
Robin Hood <yoga3911@gmail.com>
2022-07-11 18:31:54 +0700

    add new product remote for delete product by product id

--- lib/app/features/product/data/sources/product_data_remote.dart
+++ lib/app/features/product/data/sources/product_data_remote.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:dio/dio.dart';
+import 'package:my_project/app/features/product/data/models/delete_product_model.dart';
 import 'package:my_project/app/features/product/data/models/insert_product_model.dart';
 
 import '../../../../core/constants/endpoint.dart';
@@ -11,6 +12,7 @@ abstract class ProductDataSourceRemote {
   Future<List<ProductModel>> getAllData();
   Future<InsertProductModel> insertProduct(
       InsertProductModel insertProductModel);
+  Future<dynamic> deleteProduct(DeleteProductModel deleteProductModel);
 }
 
 class ProductDataSourceRemoteImpl implements ProductDataSourceRemote {
@@ -59,7 +61,8 @@ class ProductDataSourceRemoteImpl implements ProductDataSourceRemote {
     try {
       const header = {
         'Content-Type': 'application/json',
-        'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImVrbzEyMyIsImVtYWlsIjoiZWtvQGdtYWlsLmNvbSIsImFkZHJlc3MiOiJKbCBLZW5hbmdhIDEyIiwicm9sZV9pZCI6MiwiZXhwIjoxNjU3MjEyMjYzLCJpYXQiOjE2NTcxMjU4NjMsImlzcyI6Im15cHJvamVjdCJ9.R8SPUfHKK_h6ro7uDT6rA5W0IkUa4SomaFO-ZjdA5j8',
+        'Authorization':
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImVrbzEyMyIsImVtYWlsIjoiZWtvQGdtYWlsLmNvbSIsImFkZHJlc3MiOiJKbCBLZW5hbmdhIDEyIiwicm9sZV9pZCI6MiwiZXhwIjoxNjU3MjEyMjYzLCJpYXQiOjE2NTcxMjU4NjMsImlzcyI6Im15cHJvamVjdCJ9.R8SPUfHKK_h6ro7uDT6rA5W0IkUa4SomaFO-ZjdA5j8',
       };
 
       final body = jsonEncode(insertProductModel.toJson());
@@ -89,5 +92,33 @@ class ProductDataSourceRemoteImpl implements ProductDataSourceRemote {
       quantity: 0,
       description: "",
     );
+  }
+
+  //! DELETE PRODUCT
+  @override
+  Future<dynamic> deleteProduct(DeleteProductModel deleteProductModel) async =>
+      _deleteProduct(ApiURL.baseUrl + ApiURL.product, deleteProductModel);
+
+  Future<dynamic> _deleteProduct(
+      String path, DeleteProductModel deleteProductModel) async {
+    try {
+      const header = {
+        "Content-Type": "application/json",
+        'Authorization':
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImVrbzEyMyIsImVtYWlsIjoiZWtvQGdtYWlsLmNvbSIsImFkZHJlc3MiOiJKbCBLZW5hbmdhIDEyIiwicm9sZV9pZCI6MiwiZXhwIjoxNjU3NjI0OTQyLCJpYXQiOjE2NTc1Mzg1NDIsImlzcyI6Im15cHJvamVjdCJ9.vWpzfGSb_BMZ2JafirhkKzGMtvzuVbvrzxLOu8hiNjs',
+      };
+
+      final body = jsonEncode(deleteProductModel.toJson());
+
+      final response =
+          await dio.delete(path, options: Options(headers: header), data: body);
+
+      if (response.statusCode == 200) {
+        log("Delete berhasil");
+      }
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
   }
 }
