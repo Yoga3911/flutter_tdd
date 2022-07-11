35eb1311223659cd7809df81700236b5ae48de55
Robin Hood <yoga3911@gmail.com>
2022-07-11 18:29:09 +0700

    add feature to delete product by product id

--- lib/app/features/product/presentation/widgets/my_list_tile.dart
+++ lib/app/features/product/presentation/widgets/my_list_tile.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:my_project/app/features/product/data/models/delete_product_model.dart';
 import 'package:my_project/app/features/product/domain/entities/product_entity.dart';
+import 'package:my_project/app/features/product/presentation/providers/products_provider.dart';
+import 'package:provider/provider.dart';
 
 class MyListTile extends StatelessWidget {
   const MyListTile({super.key, required this.product});
@@ -9,6 +12,20 @@ class MyListTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(product.name),
+      trailing: IconButton(
+        splashRadius: 25,
+        onPressed: () {
+          context.read<ProductProvider>().deleteProduct(
+                DeleteProductModel(
+                  productId: product.id,
+                  userId: "fc8445b1-9a74-4416-969c-2c69e5dc5686",
+                ),
+              );
+        },
+        icon: const Icon(
+          Icons.delete,
+        ),
+      ),
     );
   }
 }
