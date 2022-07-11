1ca6cb579ef8f3bf536c61cd758b852d5879d70c
Robin Hood <yoga3911@gmail.com>
2022-07-11 18:30:09 +0700

    add

---
+++ lib/app/features/product/data/models/delete_product_model.dart
@@ -0,0 +1,13 @@
+import 'package:my_project/app/features/product/domain/entities/delete_product_entity.dart';
+
+class DeleteProductModel extends DeleteProductEntity {
+  const DeleteProductModel({
+    required super.productId,
+    required super.userId,
+  });
+
+  Map<String, dynamic> toJson() => {
+        "product_id": productId,
+        "user_id": userId,
+      };
+}
--- lib/app/features/product/data/models/product_model.dart
+++ lib/app/features/product/data/models/product_model.dart
@@ -2,12 +2,14 @@ import 'package:my_project/app/features/product/domain/entities/product_entity.d
 
 class ProductModel extends ProductEntity {
   const ProductModel({
+    required super.id,
     required super.name,
     required super.price,
     required super.quantity,
   });
 
   factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
+        id: json["id"],
         name: json["name"],
         price: json["price"],
         quantity: json["quantity"],
