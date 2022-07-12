2b53b01b2d0e4942802b54d852fb3a985c0d64bc
Robin Hood <yoga3911@gmail.com>
2022-07-12 23:08:07 +0700

    minor change

--- lib/app/features/product/presentation/widgets/my_txt_product.dart
+++ lib/app/features/product/presentation/widgets/my_txt_product.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../domain/entities/product_entity.dart';
 import '../providers/textfield_provider.dart';
 import 'my_text_field.dart';
 
 class MyTextProduct extends StatelessWidget {
   const MyTextProduct({super.key});
 
+  void initController(Map<String, dynamic> args, TextFieldProvider controller) {
+    if (args["product"] != null) {
+      ProductEntity product = args["product"] as ProductEntity;
+      controller.productName.text = product.name;
+      controller.price.text = product.price.toString();
+      controller.quantity.text = product.quantity.toString();
+      controller.description.text = product.description;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final controller = context.read<TextFieldProvider>();
+    final args =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    initController(args, controller);
     return Padding(
       padding: const EdgeInsets.symmetric(
         horizontal: 30,
