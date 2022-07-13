ab8bc9a40cc14e6af12729be43884f24600ec2e2
Robin Hood <yoga3911@gmail.com>
2022-07-13 23:17:56 +0700

    tidy up

--- lib/app/features/product/domain/repositories/product_data_repository.dart
+++ lib/app/features/product/domain/repositories/product_data_repository.dart
@@ -1,9 +1,9 @@
 import 'package:dartz/dartz.dart';
-import 'package:my_project/app/features/product/data/models/delete_product_model.dart';
-import 'package:my_project/app/features/product/data/models/insert_product_model.dart';
-import 'package:my_project/app/features/product/domain/entities/product_entity.dart';
 
 import '../../../../core/exceptions/failures.dart';
+import '../../data/models/insert_product_model.dart';
+import '../../data/models/delete_product_model.dart';
+import '../../domain/entities/product_entity.dart';
 import '../entities/insert_product_entity.dart';
 
 abstract class ProductDataRepository {
