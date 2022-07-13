569c4763f0ebb62585ccbe633c493fc9a233cdee
Robin Hood <yoga3911@gmail.com>
2022-07-13 23:17:24 +0700

    tidy up

--- lib/app/core/providers/init.dart
+++ lib/app/core/providers/init.dart
@@ -1,7 +1,7 @@
-import 'package:my_project/app/features/product/presentation/providers/products_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
+import '../../features/product/presentation/providers/products_provider.dart';
 class MyProvider {
   MyProvider._();
 
